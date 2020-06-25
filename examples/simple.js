f35b73d761cf15f3de2b349592f71b78cc8af6a0
Ryan McNeely <rmcneely23@gmail.com>
2020-06-24 21:37:22 -0400

    Redo simple class example to not use class fields

--- examples/simple.js
+++ examples/simple.js
@@ -3,22 +3,24 @@
  * @name Simple
  */
 class Simple {
-    /**
-     * Sample property on our class. Prop 'foo' is true!
-     * @type {boolean}
-     * @memberof Simple#
-     */
-    foo = true
-
-    /**
-     * Sample method that can be called by our class. 
-     * Completely contrived but shows arrow function and default argument
-     * @param {*} required A necessary first argument
-     * @param {string} [param.optional = string]
-     * @memberof Simple#
-     */
-    check = (required, optional='nice to have') => {
-        if(!required) throw new Error('required param is required!')
-        console.log(optional)
+    constructor() {
+        /**
+         * Sample property on our class. Prop 'foo' is true!
+         * @type {boolean}
+         * @memberof Simple#
+         */
+        this.foo = true
+        
+        /**
+         * Sample method that can be called by our class. 
+         * Completely contrived but shows arrow function and default argument
+         * @param {*} required A necessary first argument
+         * @param {string} [param.optional = string]
+         * @memberof Simple#
+         */
+        this.check = (required, optional='nice to have') => {
+            if(!required) throw new Error('required param is required!')
+            console.log(optional)
+        }
     }
 }
